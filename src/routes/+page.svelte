0a0c08be40bb7c37629c12b4048bbea4080c7af4
Michael Nix <michael.g.nix@gmail.com>
2024-09-25 20:14:10 -0500

    Added temp message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,10 @@
-<h1>We're using SvelteKit for this now! This should all update properly.</h1>
+<h1>UNDER CONSTRUCTION</h1>
+<h3>Hello!</h3>
+
+<p>
+	If you're reading this, you've caught me in the middle of setting this site up.
+	Eventually this site will be a showcase of my personal projects, along with tutorials, demos, blog posts, or whatever else I feel like making.
+
+	For now, if you're looking for information about me, my GitHub is probably the best place to look.
+</p>
+
