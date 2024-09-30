4b31d0c68e9fb31d1db4a231a4a9c44aa82f62ca
Michael Nix <michael.g.nix@gmail.com>
2024-09-30 15:42:49 -0500

    Added sass support

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -8,13 +8,16 @@
 
 </script>
 
-<div class={mode}>
+<div class={mode}><a href={"/projects/" + project.id}>
     <img src={project.icon} alt={project.name + " Icon"}/>
-    <h1><a href={"/projects/" + project.id}>{project.name}</a></h1>
-    <p>{project.description}</p>
-</div>
+    <hgroup>
+        <h1>{project.name}</h1>
+        <p>{project.description}</p>
+    </hgroup>
 
-<style lang="css">
+</a></div>
+
+<style lang="scss">
     
     div {
         border: 1px solid black;
@@ -24,8 +27,12 @@
         overflow: hidden;
     }
 
+    hgroup {
+        padding: 5px 10px;
+    }
+
     .grid {
-        aspect-ratio: 0.75;
+        /* aspect-ratio: 0.75; */
         width: 20%;
     }
 
@@ -41,5 +48,19 @@
         object-fit: cover
     }
 
+    h1 {
+        padding: 0;
+        margin: 0;
+    }
+
+    p {
+        padding: 0;
+        margin: 0;
+    }
+
+    a {
+        color: var(--text-color);
+        text-decoration: none;
+    }
 
 </style>
