859dd99f1d5b583df7f23901af926e31a030e743
Michael Nix <michael.g.nix@gmail.com>
2024-10-07 10:46:53 -0500

    Wrote temp test icon code

---
+++ src/lib/assets/icons/SVGIcon.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+
+export let raw: string;
+
+</script>
+
+{@html raw}
---
+++ src/lib/assets/icons/github-mark/GitHubIcon.svelte
@@ -0,0 +1,7 @@
+<script>
+    import SvgIcon from "../SVGIcon.svelte";
+
+    import icon from "./github-mark.svg?raw"
+</script>
+
+<SvgIcon raw={icon}/>
