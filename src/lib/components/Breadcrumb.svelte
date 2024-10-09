da2a354d133ab5a091f1bc6b413cbae0c1fa1ae6
Michael Nix <michael.g.nix@gmail.com>
2024-10-08 20:51:38 -0500

    created basic breadcrumb

---
+++ src/lib/components/Breadcrumb.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import type { LabeledLink } from "$lib/types/types";
+
+	export let crumbs: LabeledLink[];
+</script>
+
+<div>
+	{#each crumbs as crumb}
+		<a href={crumb.url}>{crumb.name}</a>
+	{/each}
+</div>
