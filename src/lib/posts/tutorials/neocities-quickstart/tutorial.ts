ad9b75539cec93a183cf11d6fa176fe22c06b417
Michael Nix <michael.g.nix@gmail.com>
2024-10-14 16:40:26 -0500

    Updated tutorial

--- src/lib/posts/tutorials/neocities-quickstart/tutorial.ts
+++ src/lib/posts/tutorials/neocities-quickstart/tutorial.ts
@@ -1,12 +1,17 @@
-import { completeTutorial, type Tutorial } from "$lib/data/tutorial"
+import { completeTutorial, fileTutorial, type Tutorial } from "$lib/data/tutorial"
 
 import HTMLSection from "./1. html/section"
+
+import introduction from "./0. introduction.md?url";
+import setup from "./1. neocities-setup.md?url";
 
 const tutorial: Tutorial = completeTutorial({
     title: "Neocities Quickstart",
     slug: "neocities-quickstart",
 
     children: [
+        fileTutorial("Introduction", "intro", introduction),
+        fileTutorial("Setting Up Neocities", "neocities-setup", setup),
         HTMLSection
     ]
 });
